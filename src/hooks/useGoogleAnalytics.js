da698e07cb843d1c09c20b75de086a3209ca61be
lharrak-botaina <92041925+lharrak-botaina@users.noreply.github.com>
2026-05-15 12:38:19 +0100

    Add Google Analytics hook for React

---
+++ src/hooks/useGoogleAnalytics.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+
+export function useGoogleAnalytics() {
+  useEffect(() => {
+    // Load Google Analytics script
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-4EWEQLGPW7';
+    document.head.appendChild(script);
+
+    // Initialize gtag
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', 'G-4EWEQLGPW7');
+  }, []);
+}
